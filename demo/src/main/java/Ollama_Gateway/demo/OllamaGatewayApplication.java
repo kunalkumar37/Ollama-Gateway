86c50e0e62a96ef5ffa1cbaf6f2ccbedf5d53b1c
kunal kumar <kumarkunal4052002@gmail.com>
2026-03-08 17:14:16 +0530

    first commit

---
+++ demo/src/main/java/Ollama_Gateway/demo/OllamaGatewayApplication.java
@@ -0,0 +1,13 @@
+package Ollama_Gateway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamaGatewayApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/Ollama_Gateway/demo/OllamaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package Ollama_Gateway.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
