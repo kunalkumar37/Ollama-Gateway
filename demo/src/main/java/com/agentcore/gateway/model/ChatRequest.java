1fabe1f71f3bd8ee7bb3138d136d7d072c309260
kunal kumar <kumarkunal4052002@gmail.com>
2026-03-09 23:29:50 +0530

    Refactor ChatRequest class to include Lombok annotations and maintain package structure

--- demo/src/main/java/com/agentcore/gateway/model/ChatRequest.java
+++ demo/src/main/java/com/agentcore/gateway/model/ChatRequest.java
@@ -1,3 +1,10 @@
-public class ChatRequest {
+package com.agentcore.gateway.model;
 
+import lombok.Data;
+
+@Data 
+public class ChatRequest {
+    private String model;
+    private String prompt;
+    
 }
